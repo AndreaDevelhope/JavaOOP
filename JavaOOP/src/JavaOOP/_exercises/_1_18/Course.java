26871ed85bccd4a5bd287649ec8fc4494f1b222e
AndreaDevelhope <127102367+AndreaDevelhope@users.noreply.github.com>
2023-03-20 14:50:50 +0100

    esercizio 1

---
+++ JavaOOP/src/JavaOOP/_exercises/_1_18/Course.java
@@ -0,0 +1,15 @@
+package JavaOOP._exercises._1_18;
+
+
+import java.util.List;
+
+public class Course {
+    String courseName;
+    int maxStudents;
+    double qualityRatingOutOf10;
+    int durationInDays;
+    List<String> studentNames;
+    List<Student> student;
+}
+
+
---
+++ JavaOOP/src/JavaOOP/_exercises/_1_18/Student.java
@@ -0,0 +1,14 @@
+package JavaOOP._exercises._1_18;
+
+public class Student {
+    String name;
+    int age;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
